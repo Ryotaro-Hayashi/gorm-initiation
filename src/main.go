c71e1dceb75256abaee7657d5f149bd881ee3e6d
林　遼太朗 <hys.rtr.forit@gmail.com>
2020-06-09 23:32:04 +0900

    Add：gormでmysqlに接続

--- src/main.go
+++ src/main.go
@@ -1,1 +1,25 @@
 package main
+
+import (
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"log"
+)
+
+type Product struct {
+	gorm.Model
+	Code string
+	// 64bitの符号なし整数
+	Price uint
+}
+
+func main() {
+	db, err := gorm.Open("mysql", "root:rootpass@tcp(mysql:3306)/gorm_db")
+	if err != nil {
+		panic("cannot connect db.")
+	}
+
+	log.Print("success in connecting db.")
+
+	log.Print("%T", db)
+}
